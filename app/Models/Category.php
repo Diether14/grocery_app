7f5b29c00c6e6addb449d697841519463d758330
Diether <llenaresdiether@gmail.com>
2020-09-27 21:05:24 +0800

    configured model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+        //table for category model
+        protected $table = 'categories';
+
+        //fillable fields for category model
+        protected $fillable = ['name', 'description', 'price', 'status'];
+    
+        //relationship configurations
+        public function items() {
+            return $this->belongsToMany('App\Models\Item', 'category_items', 'category_id', 'item_id');
+        }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    //table for item model
+    protected $table = 'items';
+
+    //fillable fields for item model
+    protected $fillable = ['name', 'description', 'price', 'status'];
+
+    //relationship configurations
+    public function categories() {
+        return $this->belongsToMany('App\Models\Category', 'category_items', 'item_id', 'category_id');
+    }
 }
