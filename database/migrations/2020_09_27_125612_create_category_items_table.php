109275e5e567b6f0e06eaa4c05a99ba3164be472
Diether <llenaresdiether@gmail.com>
2020-09-27 20:57:37 +0800

    added migrations

---
+++ database/migrations/2020_09_27_125612_create_category_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_items', function (Blueprint $table) {
+            $table->integer('item_id');
+            $table->integer('category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_items');
+    }
+}
